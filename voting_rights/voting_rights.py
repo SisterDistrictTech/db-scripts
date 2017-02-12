e35e6aa94688916b1501da0a346801b812bb7a76
Augie Hill <augman85@gmail.com>
2017-02-12 14:13:18 -0800

    Switch 2017 to 2016.

--- voting_rights/voting_rights.py
+++ voting_rights/voting_rights.py
@@ -5,11 +5,11 @@ out = open("voting_rights/voting_rights.sql", "w+")
 out.write("USE sisterdistrict_dev;\n")
 out.write("TRUNCATE TABLE voting_rights;\n")
 
-voting_rights_2017 = csv.reader(open("./voting_rights/voting_rights_2017.csv"))
-voting_rights_2017_header = next(voting_rights_2017)
+voting_rights_2016 = csv.reader(open("./voting_rights/voting_rights_2016.csv"))
+voting_rights_2016_header = next(voting_rights_2016)
 
-for row in voting_rights_2017:
-  columns = ",".join(voting_rights_2017_header)
+columns = ",".join(voting_rights_2016_header)
+for row in voting_rights_2016:
   values = ",".join(['"' + col.replace('"', '\\"') + '"' for col in row])
   out.write("INSERT INTO voting_rights (" + columns + ") VALUES (" + values + ");\n")
 
