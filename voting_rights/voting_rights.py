22bade5d13e85d1b064e1ec39b0ef83c4ff0dc24
Bob Glickstein <bobg@emphatic.com>
2017-03-03 19:48:51 -0800

    Miscellaneous small fixes for problems encountered during initial setup.

--- voting_rights/voting_rights.py
+++ voting_rights/voting_rights.py
@@ -1,11 +1,10 @@
 import csv
 
-out = open("voting_rights/voting_rights.sql", "w+")
+out = open("voting_rights.sql", "w+")
 
-out.write("USE sisterdistrict_dev;\n")
 out.write("TRUNCATE TABLE voting_rights;\n")
 
-voting_rights_2016 = csv.reader(open("./voting_rights/voting_rights_2016.csv"))
+voting_rights_2016 = csv.reader(open("voting_rights_2016.csv"))
 voting_rights_2016_header = next(voting_rights_2016)
 
 columns = ",".join(voting_rights_2016_header)
