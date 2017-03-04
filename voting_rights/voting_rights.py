a1989beb6a6fc4ad8be1d013c6344e9004b292d9
Bob Glickstein <bobg@emphatic.com>
2017-03-03 22:13:26 -0800

    Oops, in Python 3 print is a function.

--- voting_rights/voting_rights.py
+++ voting_rights/voting_rights.py
@@ -3,7 +3,7 @@ import sys
 
 out = open("voting_rights.sql", "w+")
 
-print "TRUNCATE TABLE voting_rights;"
+print("TRUNCATE TABLE voting_rights;")
 
 voting_rights_2016 = csv.reader(sys.stdin)
 voting_rights_2016_header = next(voting_rights_2016)
@@ -12,4 +12,4 @@ columns = ",".join(voting_rights_2016_header)
 for row in voting_rights_2016:
   values = ",".join(['"' + col.replace('"', '\\"') + '"' for col in row])
   # TODO: sanitize db input. remember little bobby tables...
-  print "INSERT INTO voting_rights (%s) VALUES (%s)" % (columns, values)
+  print("INSERT INTO voting_rights (%s) VALUES (%s)" % (columns, values))
