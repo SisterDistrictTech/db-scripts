5f2f82a66512a6f1208f414e6d4056d494453d0a
LinneaHarts <linneah@gmail.com>
2017-03-15 13:40:37 -0400

    Removed drop and create

--- SD_DB_Setup.sql
+++ SD_DB_Setup.sql
@@ -1,7 +1,3 @@
-drop database if exists SisterDistrict_dev;
-  
-create database SisterDistrict_dev;
-
 CREATE TABLE national_districts
 	(id int(11) NOT NULL auto_increment primary key,
     state varchar(2) NOT NULL,
